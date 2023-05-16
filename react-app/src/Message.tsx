3f1a04a53169e46b1b290a591f8b7277b7d1f1c5
KaidoShmaido <spoofilstws12@gmail.com>
2023-05-16 10:35:00 +0100

    installed bootstrap

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,13 @@
+
+//PascalCasing = is a programming naming convention where the first letter of each compound word in a variable is capitalized.
+
+function Message(){
+    //JSX:Javascript XML
+    const name ="Kaido";
+    if (name)
+    return <h1>hello world {name}</h1>;
+  return <h1>hello world </h1>
+
+}
+
+export default Message;
